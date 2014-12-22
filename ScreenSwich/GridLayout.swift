217434a13ddbff613e9f174b469ce78ae912dbb2
wangjun <marcus.jun@qq.com>
2014-12-22 10:09:37 +0800

    first commit

---
+++ ScreenSwich/GridLayout.swift
@@ -0,0 +1,13 @@
+//
+//  GridLayout.swift
+//  ScreenSwich
+//
+//  Created by 王 军 on 14/12/8.
+//  Copyright (c) 2014年 王 军. All rights reserved.
+//
+
+import UIKit
+
+class GridLayout: UICollectionViewFlowLayout {
+   
+}
---
+++ ScreenSwich/PictureCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PictureCollectionViewCell.swift
+//  ScreenSwich
+//
+//  Created by 王 军 on 14/12/4.
+//  Copyright (c) 2014年 王 军. All rights reserved.
+//
+
+import UIKit
+
+class PictureCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
